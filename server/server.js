3a8a607520b2a654c7178ef24fdf101920cedb43
ishitaAg08 <64578556+ishitaAg08@users.noreply.github.com>
2021-07-10 23:33:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
+//Loading dependencies & initializing express
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-const http = require("http")
+const http = require("http")//for creating http server
 const app = express()
-const server = http.createServer(app)
-const io = require("socket.io")(server)
+const server = http.createServer(app) //Initialize http server and associate it with express
+//Initialize socket.io
+const io = require("socket.io")(server) //For signalling in WebRTC
 
 app.use(cors())
+
+//Define a route
 app.get("/", (req, res) => {
 	res.send({ response: "Server is up and running." }).status(200);
   });
+
+
+//Implementing Socket.io
+//connection event is fired as soon as a client connects to this socket.
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id)
 	
@@ -26,5 +34,6 @@ io.on("connection", (socket) => {
 	})
 })
 
+//Ports on which server should listen - 8000 or the one provided by the environment
 const port= process.env.PORT || 5000;
 server.listen(port, () => console.log(`server is running on port ${port}`))
